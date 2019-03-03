53d5536e510d3cf194a85f2636e5f02bc970e5fc
odzhan <odzhan@protonmail.com>
2019-03-03 20:05:10 +0000

    upd

--- hash/crc/crc.c
+++ hash/crc/crc.c
@@ -1,64 +1,60 @@
 
 #include "crc.h"
 
-// crc32 code derived from code here: https://crccalc.com/
+// crc code derived from code here: https://crccalc.com/
 
-uint32_t crc_table[256];
+static uint64_t crc_table[256];
 
-uint32_t reflectx(uint32_t x) {
-    uint32_t r = 0;
-    int      i;
+static uint64_t reflect(uint64_t bitlen, uint64_t x) {
+    uint64_t i, r = 0;
     
-    for(i=0; i<32; i++) {
-      if((x & (1 << i)) != 0) {
-        r |= ((1 << (31 - i)) & 0xFFFFFFFF);
+    for(i=0; i<bitlen; i++) {
+      if((x & (1ULL << i)) != 0) {
+        r |= ((1ULL << (bitlen - i - 1)));
       }
     }
     return r;
 }
-
-uint32_t reflect(uint32_t x) {
-    x = ((x & 0x55555555) << 1) | ((x >> 1) & 0x55555555);
-    x = ((x & 0x33333333) << 2) | ((x >> 2) & 0x33333333);
-    x = ((x & 0x0F0F0F0F) << 4) | ((x >> 4) & 0x0F0F0F0F);
-    x = (x << 24) | ((x & 0xFF00) << 8) | ((x >> 8) & 0xFF00) | (x >> 24);
-
-    return x;
-}
+ 
+static void create_table(crc_param *p) {
+    int      j;
+    uint64_t i, r, m=~0ULL;
     
-void create_table(crc_param *p) {
-    int      i, j;
-    uint32_t r;
+    if(p->bitlen<64) m = (1ULL << p->bitlen) - 1;
     
     for(i=0; i<256; i++) {
-      r = (p->refin) ? reflect(i) : i << 24;
+      r = (p->refin) ? reflect(p->bitlen, i) : i << (p->bitlen - 8);
 
       for (j=0; j<8; j++) {
-        if ((r & 0x80000000) != 0) {
-          r = ((r << 1) ^ p->poly);
+        if (r & (1ULL << (p->bitlen - 1))) {
+          r = ((r << 1ULL) ^ p->poly);
         } else {
-          r <<= 1;
+          r <<= 1ULL;
         }
       }
-      crc_table[i] = (p->refout) ? reflect(r) : r;
+      r = (p->refout) ? reflect(p->bitlen, r) : r;
+      crc_table[i] = (r & m);
     }
 }
 
-uint32_t crc32(const void *input, size_t len, crc_param *p) {
-    uint32_t crc;
+uint64_t crc(const void *input, size_t len, crc_param *p) {
+    uint64_t crc, m=~0ULL;
     uint8_t  *data=(uint8_t*)input;
     int      i;
     
+    if(p->bitlen<64) m = (1ULL << p->bitlen) - 1;
+
     create_table(p);
     
-    crc = p->refin ? reflect(p->iv) : p->iv;
+    crc = p->refin ? reflect(p->bitlen, p->iv) : p->iv;
     
     for(i=0; i<len; i++) {
       if (p->refout) {
         crc = (crc >> 8) ^ crc_table[(crc&0xFF)^data[i]];
       } else {
-        crc = (crc << 8) ^ crc_table[(crc>>24)^data[i]];  
+        crc = (crc << 8) ^ crc_table[(crc>>(p->bitlen-8))^data[i]];  
       }
+      crc &= m;
     }
-    return (crc ^ p->xor);
+    return (crc ^ p->xor) & m;
 }
